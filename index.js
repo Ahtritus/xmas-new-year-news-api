555ef32c610ae90cd5233be8017f750820714e1c
Ahtritus <sutirthabharatidas@gmail.com>
2021-12-21 22:16:19 +0530

    added axios

--- index.js
+++ index.js
@@ -3,12 +3,41 @@
 const PORT = process.env.PORT || 3000;
 
 const express = require("express");
-
+const axios = require("axios");
 const app = express();
 
+const newspapers = [
+  {
+    name: "The Telegraph India",
+    address: "https://www.telegraphindia.com/search?keyword=christmas",
+  },
+  {
+    name: "The Times Of India",
+    address: "https://timesofindia.indiatimes.com/topic/Christmas",
+  },
+];
+
+const articles = [];
+
+newspapers.forEach((newspaper) => {
+  axios
+    .get(newspaper.address)
+    .then((response) => {
+      const data = response.data;
+      articles.push(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 app.get("/testPath", (req, res) => {
   console.log(req);
   res.send("Welcome to the API");
+});
+
+app.get("/articles", (req, res) => {
+  console.log(req);
+  res.json(articles);
 });
 
 app.listen(PORT, () => {
